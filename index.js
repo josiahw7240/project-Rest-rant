08d8a201fb86140a175678d26c0a6df42414f616
josiahw7240 <71360953+josiahw7240@users.noreply.github.com>
2022-03-22 13:09:36 -0400

    adding remaining stub routes

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const app = express();
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello world!");
 });
 
-// 404
 app.get("*", (req, res) => {
   res.status(404).send("<h1>404 Page</h1>");
 });
