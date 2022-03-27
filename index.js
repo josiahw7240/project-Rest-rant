6a273310ee476ee62673bd351b0c9a6704c3ecf8
josiahw7240 <71360953+josiahw7240@users.noreply.github.com>
2022-03-27 17:07:44 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine("jsx", require("express-react-views").createEngine());
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.send("home");
+  res.send("error404");
 });
 
 app.get("*", (req, res) => {
